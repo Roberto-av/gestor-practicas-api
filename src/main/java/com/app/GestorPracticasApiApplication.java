f71e6304aedd5216a8741dc41102a84ad5efd0b0
Roberto Aviles <avilesrobertoe@gmail.com>
2024-05-21 13:38:00 -0700

    first commit

---
+++ src/main/java/com/app/GestorPracticasApiApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorPracticasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorPracticasApiApplication.class, args);
+	}
+
+}
